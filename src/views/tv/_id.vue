25afd9226a675aea80a2073eeb1d90f4a24372da
HesamTarighi <hesammp86@gmail.com>
2023-11-20 16:50:54 +0330

    episodes component ->  async component:On progress

--- src/views/tv/_id.vue
+++ src/views/tv/_id.vue
@@ -8,10 +8,7 @@
                         <MTDetails :data="details" />
                     </template>
                     <template #fallback>
-<<<<<<< HEAD
                         <Loading class="absolute x-center" />
-=======
->>>>>>> 3270a4594b36f7cbdb9dc7b73ebc4a40477226fe
                     </template>
                 </Suspense>
                 <!-- trailers -->
@@ -26,9 +23,10 @@
                 <!-- episodes -->
                 <Suspense>
                     <template #default>
-                        <TvEpisodes :details="details" @getSeason="callSeason" :season="season" :id="id" />
+                        <TvEpisodes :details="details" @getSeason="callSeasonApi" :season="season" :id="id" />
                     </template>
                     <template #fallback>
+                        <Loading class="absolute x-center" />
                     </template>
                 </Suspense>
                 <!-- cast -->
@@ -43,10 +41,6 @@
 <script setup>
     // components
     import Layout from '@/layouts/default.vue'
-<<<<<<< HEAD
-=======
-    import TvTrailers from '@/components/sections/tv/Trailers.vue'
->>>>>>> 3270a4594b36f7cbdb9dc7b73ebc4a40477226fe
     import TvEpisodes from '@/components/sections/tv/Episodes.vue'
     import MTCast from '@/components/sections/mt/Cast.vue'
     import MTSimilars from '@/components/sections/mt/Similars.vue'
@@ -63,13 +57,16 @@
     const id = route.params.id
     // response data
         const details = ref({})
-<<<<<<< HEAD
         const trailers = ref({})
+        const season = ref([])
 
     // call api
     const callDetailsApi = tv.getDetails(id).then(response => Object.assign(details.value, response.data))
     const callContentRatingApi = tv.getContentRating(id).then(response => Object.assign(details.value, { content_rating: response.data }))
     const callVideosApi = tv.getVideos(id).then(response => trailers.value = response.data)
+    function callSeasonApi (selectedSeason) {
+        return tv.getSeason(id, selectedSeason).then(response => season.value = response.data)
+    }
 
     // define async components
     const MTDetails = defineAsyncComponent(() => {
@@ -77,19 +74,5 @@
     })
     const TvTrailers = defineAsyncComponent(() => {
         return Promise.all([callVideosApi]).then(() => import('@/components/sections/tv/Trailers.vue'))
-=======
-        const season = ref([])
-
-    // call api
-    const callDetails = tv.getDetails(id).then(response => Object.assign(details.value, response.data))
-    const callContentRating = tv.getContentRating(id).then(response => Object.assign(details.value, { content_rating: response.data }))
-    function callSeason (selectedSeason) {
-        return tv.getSeason(id, selectedSeason).then(response => season.value = response.data)
-    }
-
-    // define async components
-    const MTDetails = defineAsyncComponent(() => {
-        return Promise.all([callDetails, callContentRating]).then(() => import('@/components/sections/mt/Details.vue'))
->>>>>>> 3270a4594b36f7cbdb9dc7b73ebc4a40477226fe
     })
-</script>
+;</script>
